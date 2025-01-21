e9c450036e91a33cc3c36117d0ed6add91d5f21b
Blas <montanariblas@hotmail.com>
2025-01-21 01:12:38 -0300

    vercel fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 
-import Navbar from "./components/Navbar/Navbar";
+// import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
 import Section1 from "./components/Section1/Section1";
 import Section2 from "./components/Section2/Section2";
-import { Canvas } from "@react-three/fiber";
-import Fiber from "./components/R3F/fiber";
-import GridTest from "./components/GridTest/GridTest";
-import GridTest2 from "./components/GridTest2/GridTest2";
+// import { Canvas } from "@react-three/fiber";
+// import Fiber from "./components/R3F/fiber";
+// import GridTest from "./components/GridTest/GridTest";
+// import GridTest2 from "./components/GridTest2/GridTest2";
 import Section3 from "./components/Section3/Section3";
 
 import bgVector from "./resources/bg-vector.png";
