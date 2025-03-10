dd738be922eeff8ddff98cecf55e9947cedb2569
Blas <montanariblas@hotmail.com>
2025-03-09 23:41:10 -0300

    zust up

--- src/store.js
+++ src/store.js
@@ -4,6 +4,6 @@ import { create } from 'zustand'
 export const useRescuesStore = create((set) => ({
   rescates: "+5000",
   updateRescates: (newRescates) => {
-    set({ rescates: newRescates }),
+    set({ rescates: newRescates })
   }
 }))
